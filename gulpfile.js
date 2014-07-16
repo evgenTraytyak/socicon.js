fad1f94c6bd2c2c00ca6aa3da58c649ef97875bb
Evgeny Traytyak <evgen.traytyak@gmail.com>
2014-07-16 12:46:34 +0300

    Add gulp-csso. Add gulp-watch.  Add 'build' gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var 
 	gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	prefix = require('gulp-autoprefixer');
+	prefix = require('gulp-autoprefixer'),
+	watch = require('gulp-watch'),
+	csso = require('gulp-csso');
 
 gulp.task('sass', function () {
 	gulp
@@ -14,4 +16,19 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./source/css/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('watch', function() {
+	gulp.watch('./dev/css/**', function() {
+		gulp.run('sass');
+	});
+});
+
+gulp.task('css-minify', function() {
+	gulp
+		.src( './source/css/*.css' )
+		.pipe( csso() )
+		.pipe( gulp.dest('./production/css/') );
+});
+
+gulp.task('default', ['sass', 'watch']);
+
+gulp.task('build', ['sass', 'css-minify']);
